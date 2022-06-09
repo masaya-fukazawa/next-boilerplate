b6a0d03ff678278a86261048ca29975e5097c696
masaya-fukazawa <masaya.fukazawa@bizreach.co.jp>
2022-06-09 16:15:57 +0900

    add esbuild

--- next.config.js
+++ next.config.js
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
+const {ESBuildMinifyPlugin} = require('esbuild-loader')
+
 module.exports = {
   reactStrictMode: true,
+  webpack: (config, {webpack}) => {
+    config.plugins.push(
+      new webpack.ProvidePlugin({
+        React: 'react',
+      }),
+    )
+    config.optimization.minimizer.push(new ESBuildMinifyPlugin({
+      target: 'es2015',
+      css: true
+    }))
+    config.module.rules.push({
+      test: /\.tsx?$/,
+      loader: 'esbuild-loader',
+      options: {
+        loader: 'tsx',
+        target: 'es2015',
+        tsconfigRaw: require('./tsconfig.json')
+      }
+    })
+    return config
+  },
 }
